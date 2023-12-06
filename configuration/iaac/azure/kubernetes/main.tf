0c3c69963da7c5ba681555a1896c10fe835b5ff9
Anilgp1243 <anilgp1991@gmail.com>
2023-12-06 15:59:28 +0530

    added vm size

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -26,7 +26,8 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     name            = "agentpool"
     node_count      = var.node_count
     #vm_size         = "standard_DS1_v2"
-    vm_size         = "standard_d2as_v5"      
+    #vm_size         = "standard_d2as_v5"   
+    vm_size         =  "standard_DS2_v2"
   }
 
   service_principal {
