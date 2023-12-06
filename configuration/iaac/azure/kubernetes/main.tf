aa7318bd5996259f18dccb4107dd08cda9bb6eeb
Anilgp1243 <anilgp1991@gmail.com>
2023-12-06 16:31:22 +0530

    updated

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_kubernetes_cluster" "terraform_k8s" {
+resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   name                = "${var.cluster_name}_${var.environment}"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
@@ -25,10 +25,10 @@ resource "azurerm_kubernetes_cluster" "terraform_k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    #vm_size         = "standard_DS1_v2"
+    vm_size         = "Standard_DS1_v2"
     #vm_size         = "standard_d2as_v5"   
     #vm_size         =  "standard_DS2_v2"
-     vm_size         = "standard_b2ms"
+     #vm_size         = "standard_b2ms"
   }
 
   service_principal {
